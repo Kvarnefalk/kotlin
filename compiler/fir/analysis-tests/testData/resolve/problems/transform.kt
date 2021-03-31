601d0561a34cd4213ff55e3b40d13136725c05d0
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-03-31 16:25:00 +0300

    ~~~ Test reproducing "inapplicable candidate" ~~~

--- compiler/fir/analysis-tests/testData/resolve/problems/transform.kt
+++ compiler/fir/analysis-tests/testData/resolve/problems/transform.kt
@@ -14,24 +14,42 @@ sealed class CompositeTransformResult<out T : Any> {
 }
 
 interface FirElement {
+    fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R
+
+    fun accept(visitor: FirVisitorVoid) = accept(visitor, null)
+
     fun <E : FirElement, D> transform(visitor: FirTransformer<D>, data: D): CompositeTransformResult<E>
 }
 
 abstract class FirVisitor<out R, in D>
 
+abstract class FirVisitorVoid : FirVisitor<Unit, Nothing?>()
+
 abstract class FirTransformer<in D> : FirVisitor<CompositeTransformResult<FirElement>, D>()
 
-interface FirAnnotationContainer : FirElement
+interface FirAnnotationContainer : FirElement {
+    abstract override fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R
+}
 
-interface FirStatement : FirAnnotationContainer
+interface FirStatement : FirAnnotationContainer {
+    abstract override fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R
+}
 
-interface FirTypeParameterRefsOwner : FirElement
+interface FirTypeParameterRefsOwner : FirElement {
+    abstract override fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R
+}
 
-interface FirDeclaration : FirElement
+interface FirDeclaration : FirElement {
+    abstract override fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R
+}
 
-interface FirAnnotatedDeclaration : FirDeclaration, FirAnnotationContainer
+interface FirAnnotatedDeclaration : FirDeclaration, FirAnnotationContainer {
+    abstract override fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R
+}
 
-interface FirSymbolOwner<E> : FirElement where E : FirSymbolOwner<E>, E : FirDeclaration
+interface FirSymbolOwner<E> : FirElement where E : FirSymbolOwner<E>, E : FirDeclaration {
+    abstract override fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R
+}
 
 interface FirClassLikeDeclaration<F : FirClassLikeDeclaration<F>> : FirAnnotatedDeclaration, FirStatement, FirSymbolOwner<F>
 
@@ -42,4 +60,31 @@ private class FirApplySupertypesTransformer() : FirTransformer<Nothing?>()
 fun <F : FirClass<F>> F.runSupertypeResolvePhaseForLocalClass(): F {
     val applySupertypesTransformer = FirApplySupertypesTransformer()
     return this.transform<F, Nothing?>(applySupertypesTransformer, null).single
+}
+
+abstract class FirPureAbstractElement : FirElement
+
+interface FirTypedDeclaration : FirAnnotatedDeclaration {
+    abstract override fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R
+}
+
+interface FirCallableDeclaration<F : FirCallableDeclaration<F>> : FirTypedDeclaration, FirSymbolOwner<F> {
+    abstract override fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R
+}
+
+abstract class FirVariable<F : FirVariable<F>> : FirPureAbstractElement(), FirCallableDeclaration<F>, FirAnnotatedDeclaration, FirStatement {
+    abstract override fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R
+}
+
+abstract class FirWhenExpression {
+    abstract val subjectVariable: FirVariable<*>?
+}
+
+class FirRenderer : FirVisitorVoid() {
+    fun foo(expression: FirWhenExpression) {
+        val variable = expression.subjectVariable
+        if (variable != null) {
+            variable.<!INAPPLICABLE_CANDIDATE!>accept<!>(this)
+        }
+    }
 }
